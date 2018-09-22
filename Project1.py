e8470310d64632a7696596acac4a882c8399dfa2
Alexander Edgar <mrcrowk@gmail.com>
2018-09-22 03:48:03 -0700

    made some empty route placeholders. Need database support and user auth to really continue

--- Project1.py
+++ Project1.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import jsonify, request
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route("/")
-def hello:
-    return "Hello World"
+@app.route("/forums", methods=['GET'])
+def view_forums:
+    # TODO: return id, name, creator, HTTP 200 OK
+    return
+
+@app.route("/forums", methods=['POST'])
+def create_forum:
+    # TODO: request name, creator; return HTTP 201 Created or HTTP 409 Conflict
+    # Set location header field to /forums/<forum_id> for new
+    return
+
+@app.route("/forums/<int:forum_id>")
+def view_threads:
+    # TODO
+    return
+
+if __name__ == "main":
+    app.run()
