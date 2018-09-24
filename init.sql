b82ff5f86fb3e0af0c74eb8e79735c802159a0de
Alexander Edgar <mrcrowk@gmail.com>
2018-09-24 07:32:23 -0700

    rename

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
 CREATE TABLE if NOT EXISTS forums (id INTEGER PRIMARY_KEY, name TEXT, creator TEXT);
+CREATE UNIQUE INDEX idx_forums ON forums(name);
 INSERT INTO forums VALUES (1, 'redis', 'alice');
 INSERT INTO forums VALUES (2, 'mongodb', 'bob');
