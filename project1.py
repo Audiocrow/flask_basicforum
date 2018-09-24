9c1860927147b724a6d34b0186fa85bd0f1030a3
Alexander Edgar <mrcrowk@gmail.com>
2018-09-24 09:17:25 -0700

    Fixed json return formats to now include the column names

--- project1.py
+++ project1.py
@@ -1,3 +1,6 @@
+#Simple forum backend project
+#Alexander Edgar
+#CPSC476
 import click
 from flask import Flask, g, jsonify, request
 from flask_basicauth import BasicAuth
@@ -6,7 +9,7 @@ import sqlite3
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#sqlite3 database setup from official Flask documentation:
+#sqlite3 database setup code provided by official Flask documentation:
 DATABASE = 'database.db'
 def get_db():
     db = getattr(g, '_database', None)
@@ -20,16 +23,30 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-#Add a custom command to Flask to initialize the db
-#Only run this once to populate the default database
 @app.cli.command()
 def init_db():
-    #Code provided by official Flask documentation:
+    #Adds a custom command to Flask to initialize the db
+    #Only run this once to populate the default database
     with app.app_context():
         db = get_db()
         with app.open_resource('init.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+def select_dicts(query : str, params=None) -> list:
+    '''Queries the database with the given SELECT statement and zips the column
+        names together with the results, then returns a list
+        of column_name:value rows'''
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        rows = cursor.execute(query, params).fetchall() if params\
+            else cursor.execute(query)
+        return [dict(zip([colname[0] for colname in cursor.description], row)) \
+            for row in rows]
+    except sqlite3.Error as e:
+        print(e)
+        return []
 
 class BasicDBAuth(BasicAuth):
     def check_credentials(username, password):
@@ -41,20 +58,14 @@ basic_auth = BasicDBAuth(app)
 @app.route("/forums", methods=['GET'])
 def view_forums():
     #Returns a list of forums
-    try:
-        db = get_db()
-        cur = db.cursor()
-        forums = cur.execute("SELECT * FROM forums;").fetchall()
-        print(forums)
-        return jsonify(forums)
-    except:
-        return jsonify([])
+    return jsonify(select_dicts("SELECT * FROM forums;"))
 
 @app.route("/forums", methods=['POST'])
 @basic_auth.required
 def create_forum():
     # TODO: request name, creator; return HTTP 201 Created or HTTP 409 Conflict
     # Set location header field to /forums/<forum_id> for new
+    print("Hello %s" % basic_auth.username())
     return
 
 @app.route("/forums/<int:forum_id>")
