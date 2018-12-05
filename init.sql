46181bd840702638c97323776c976f392a8356e6
Alexander Edgar <mrcrowk@gmail.com>
2018-12-05 10:33:21 -0800

    fixed init.sql logic - threads and posts both should have been a composite key all along

--- init.sql
+++ init.sql
@@ -7,28 +7,33 @@ CREATE UNIQUE INDEX idx_forums ON forums(name, creator);
 INSERT INTO forums VALUES (1, 'redis', 'alice');
 INSERT INTO forums VALUES (2, 'mongodb', 'bob');
 CREATE TABLE if NOT EXISTS threads (
-    id INTEGER PRIMARY KEY,
+    id INTEGER NOT NULL,
     forum INTEGER,
     title TEXT,
     creator TEXT,
     timestamp TEXT,
+    PRIMARY KEY (id, forum),
     FOREIGN KEY(forum) REFERENCES forums(id)
+        ON DELETE CASCADE
 );
-CREATE INDEX idx_threads ON threads(forum, title, creator);
+CREATE INDEX idx_threads ON threads(forum, creator);
 INSERT INTO threads VALUES (1, 1, 'Does anyone know how to start Redis?', 'bob', 'Wed, 05 Sep 2018 16:22:29 GMT');
 INSERT INTO threads VALUES (2, 1, 'Has anyone heard of Edis?', 'charlie', 'Tue, 04 Sep 2018 13:18:43 GMT');
 CREATE TABLE if NOT EXISTS posts (
-    id INTEGER PRIMARY KEY,
+    id INTEGER NOT NULL,
     thread INTEGER,
+    forum INTEGER,
     author TEXT,
     text TEXT,
     timestamp TEXT,
-    FOREIGN KEY(thread) REFERENCES threads(id)
+    PRIMARY KEY (id, thread, forum),
+    FOREIGN KEY(thread, forum) REFERENCES threads(id, forum)
+        ON DELETE CASCADE
 );
-CREATE INDEX idx_posts ON posts(author);
-INSERT INTO posts VALUES (1, 1, 'bob', 'bob placeholder text', 'Wed, 05 Sep 2018 16:22:29 GMT');
-INSERT INTO posts VALUES (2, 1, 'alice', 'snarky alice response', 'Wed, 05 Sep 2018 16:34:46');
-INSERT INTO posts VALUES (3, 2, 'charlie', 'charlie placeholder text', 'Tue, 04 Sep 2018 13:18:43 GMT');
+CREATE INDEX idx_posts ON posts(thread, forum, author);
+INSERT INTO posts VALUES (1, 1, 1, 'bob', 'bob placeholder text', 'Wed, 05 Sep 2018 16:22:29 GMT');
+INSERT INTO posts VALUES (2, 1, 1, 'alice', 'snarky alice response', 'Wed, 05 Sep 2018 16:34:46');
+INSERT INTO posts VALUES (1, 2, 1, 'charlie', 'charlie placeholder text', 'Tue, 04 Sep 2018 13:18:43 GMT');
 CREATE TABLE if NOT EXISTS users (
     username TEXT PRIMARY KEY,
     password TEXT
